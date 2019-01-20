438455865f3852c052aa381402a927fd7144d94c
befern <belen.fdez.glez@gmail.com>
2019-01-20 13:09:32 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+#Api-caller
+
+Script that calls in a loop to an endpoint with a bulk of data (generally ids)
+
+Writes the response of each call in an outfile.
+
+Logs the calls and response status to verify on the go.
+
+## Options and modifications
+
+The current script accepts OAuth2 token
+(If it's not needed it can be left as it is)
+
+The current script has a method to clean cache
+(Comment the call to the method if it is not needed)
+
+##Requirements
+
+Python3
+
+Install requests library
+
+    pip3 install requests
+
+Execute
+
+    ./api_caller.py
+   
+   
---
+++ api_caller.py
@@ -0,0 +1,30 @@
+#!/usr/local/Cellar/python/3.7.2_1/bin/python3
+import requests
+
+access_token = 'access_token_here_if_necessary'
+
+def call_api(method, id):
+    endpoint = 'https://hostname/path/' + id
+
+    request_url = endpoint + '?access_token=' + access_token
+    req = requests.request(method, request_url)
+
+    print(request_url)
+    print(req.status_code)
+
+    return str(req.content)[:100]
+
+
+def clean_cache(id):
+    request_url = 'https://hostname/path/' + id + '/cache/ban?access_token=' + access_token
+
+    req = requests.request('DELETE', request_url)
+    print(req)
+
+
+with open('./ids.txt', 'r') as infile, open('./responses.txt', 'w') as outfile:
+    for line in infile:
+        outfile.write(line)
+        clean_cache(line)
+        outfile.write(str(call_api('GET', line)))
+        outfile.write("\n")
